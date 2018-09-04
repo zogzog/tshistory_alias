4d3a2bcbc06a9bd5244492644e8d484690d5960b
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2018-09-04 14:51:39 +0200

    use real transactions

--- tshistory_alias/cli.py
+++ tshistory_alias/cli.py
@@ -12,7 +12,7 @@ from tshistory_alias import db
 def register_priorities(dburi, priority_file, override=False):
     " register priorities timeseries aliases "
     engine = create_engine(dburi)
-    with engine.connect() as cn:
+    with engine.begin() as cn:
         db.register_priority(cn, priority_file, override)
 
 
@@ -23,7 +23,7 @@ def register_priorities(dburi, priority_file, override=False):
 def register_arithmetic(dburi, arithmetic_file, override):
     " register arithmetic timeseries aliases "
     engine = create_engine(dburi)
-    with engine.connect() as cn:
+    with engine.begin() as cn:
         db.register_arithmetic(cn, arithmetic_file, override)
 
 
@@ -34,7 +34,7 @@ def register_arithmetic(dburi, arithmetic_file, override):
 def register_outliers(dburi, outliers_file, override):
     " register outlier definitions "
     engine = create_engine(dburi)
-    with engine.connect() as cn:
+    with engine.begin() as cn:
         db.register_outliers(cn, outliers_file, override)
 
 
@@ -48,5 +48,5 @@ def remove_alias(dburi, alias_type, alias, namespace='tsh'):
     engine = create_engine(dburi)
     table = '"{}-alias".{}'.format(namespace, alias_type)
     sql = "delete from {} where alias = %(alias)s".format(table)
-    with engine.connect() as cn:
+    with engine.begin() as cn:
         cn.execute(sql, alias=alias)
