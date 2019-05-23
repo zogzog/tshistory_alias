6562c8a6ad7f7fd8a68677d66c93fc7a1fa11509
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-05-23 14:10:38 +0200

    test/conf: indicate to postgres the insertion dates are in utc

--- conftest.py
+++ conftest.py
@@ -15,7 +15,10 @@ DATADIR = Path(__file__).parent / 'test' / 'data'
 @pytest.fixture(scope='session')
 def engine(request):
     port = 5433
-    db.setup_local_pg_cluster(request, DATADIR, port)
+    db.setup_local_pg_cluster(request, DATADIR, port, {
+        'timezone': 'UTC',
+        'log_timezone': 'UTC'
+    })
     uri = 'postgresql://localhost:{}/postgres'.format(port)
     e = create_engine(uri)
     sch = alias_schema()
