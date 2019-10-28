f4f70da75852770e94056ba169ec0485fc0e0d0b
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-10-28 10:49:16 +0100

    pkg: include sql file

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(name='tshistory_alias',
       author_email='arnaud.campeas@pythonian.fr, aurelien.campeas@pythonian.fr',
       description='Computed timeseries on top of the `tshistory` package',
 
+      zip_safe=False,
       packages=['tshistory_alias'],
+      package_data={'tshistory_alias': [
+          'schema.sql'
+      ]},
       install_requires=['tshistory'],
       entry_points={'tshistory.subcommands': [
           'register-priorities=tshistory_alias.cli:register_priorities',
